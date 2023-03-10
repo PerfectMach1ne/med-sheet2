8bd1e98e20763dcc15eb89b5c4ad31fe60377cfc
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-03-10 22:14:30 +0100

    feat: standard data parsing p1

--- app/utils.py
+++ app/utils.py
@@ -3,3 +3,19 @@ def map_column(col):
     column_ids = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
                   'U', 'V', 'W', 'X', 'Y', 'Z']
     return column_ids.index(col.upper()) + 1
+
+
+def stotupdts():  # string to tuple of date strings
+    pass
+
+
+def stotuphrs():  # string to tuple of hour strings
+    pass
+
+
+def stodict():  # string to dictionary
+    pass
+
+
+def stodicttuphrs():  # string to dictionary of tuples of hour strings
+    pass
